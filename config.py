cd9eb7e085c8bebc14d6d511d6cad4f2e9538e3a
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-08-01 10:28:18 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Dibuat otomatis oleh installer
+BOT_TOKEN = "8280992531:AAEDsDKtvYEcIR2uXP8aSrlAyzlhrvUzt6I"
+ADMIN_TELEGRAM_ID = 1469244768
+
+# --- KREDENSIAL DATABASE ---
+DB_HOST = 'localhost'
+DB_USER = 'cekotmyi_ppob'
+DB_PASSWORD = '1ndosaTaku'
+DB_NAME = 'cekotmyi_ppob'
+
+# --- KREDENSIAL API ---
+KMSP_API_KEY = 'e6ee99e0-c853-45fb-b1fc-d8cd8a6da062'
